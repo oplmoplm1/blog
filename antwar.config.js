4601c7012fff4b9cca7fb6de554cdacb546c9629
liyang1 <liyang1@codoon.com>
2015-07-21 00:45:12 +0800

    initial

---
+++ antwar.config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var rssPlugin = require('antwar-rss-plugin');
+
+module.exports = {
+  output: 'build',
+  name: 'blog',
+  author: {
+    name: 'swirling',
+    email: 'oplmoplm2@gmail.com'
+  },
+  deploy: {
+    branch: 'gh-pages',
+  },
+  plugins: [
+    rssPlugin(),
+  ],
+  theme: {
+    name: 'antwar-default-theme',
+    navigation: [
+      {title: 'Home', url: '/'},
+      {title: 'Blog', url: '/blog'}
+    ],
+    analyticsId: 'UA-XXXXXX-1',
+    customStyles: 'specific.scss'
+  },
+  paths: {
+    '/': {
+      path: function() {
+        return require.context('./pages');
+      }
+    },
+    blog: {
+      title: 'Blog posts',
+      layout: 'blog',
+      path: function() {
+        return require.context('./posts', true, /^\.\/.*\.md$/);
+      },
+      draft: function() {
+        return require.context('./drafts', true, /^\.\/.*\.md$/);
+      },
+    }
+  },
+};
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Note extends React.Component {
+  render() {
+    return (
+      <div className='post post--front'>
+        <h1 className='post__heading'>This is my home page!</h1>
+        <p>Just one page has not been edited, the first vision is just for fun.</p>
+      </div>
+    );
+  }
+}
